0f958a6e5ff2f75eff2db3e6c7b5d2d6b3271350
Mat <matstover@gmail.com>
2018-04-23 21:36:55 -0500

    third push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var pinkGemNumbe;
 var yellowGemNumber;
 var greenGemNumber;
 var blueGemNumber;
+var reset;
 
 // Press Start to Start
 
@@ -14,21 +15,36 @@ var blueGemNumber;
 (pinkGemNumber) = Math.floor((Math.random() * 12) + 1);
 (yellowGemNumber) = Math.floor((Math.random() * 12) + 1);
 console.log(greenGemNumber)
-// Randomly generate goal score 19-120
-(goalScore)= Math.floor((Math.random()*120)+1)
+
+
+    // Randomly generate goal score 19-120
+    (goalScore) === Math.floor((Math.random() * 19) + 1) <= (goalScore) === Math.floor((Math.random() * 19 + 120))
 console.log(goalScore)
 // Upon crystal clickup add that number to the score
+function blueGemNumber() {
+    var onmouseup (Math.floor((Math.random() * 12) + 1);
+}
+console.log(blueGemNumber)
+
+$("blueGemNumber").on("click", function ();
+$("greenGemNumber").on("click", function ();
+$("pinkGemNumber").on("click", function ();
+$("yellowGemNumber").on("click", function ();
 
 // if/else loop to check if new score is less then, equal to, or over score
 
 // ----if less than continue game
-if (shownScore) < hiddenScore true return false
+// if (shownScore) === hiddenScore true; return false;
 // ----if/else equal to hidden number add one to the win score
-if (shownScore) ===  {
-    
-} else {
-    
-}
+// if (shownScore) ===  {
+
+// } else {
+
+// }
 // ----if/else if over hidden number (game over) add one to lose score
 
 // If game over reset with start button clickup
+$("#reset").on("click", function () {
+    total = 0;
+    totalSpan.text(0);
+});
